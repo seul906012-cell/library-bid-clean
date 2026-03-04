cc5be73afdaa6772812ece5b29d8f078e079d459
seul906012-cell <seul906012@gmail.com>
2026-03-04 13:09:03 +0900

    Update route.js

--- app/api/bids/route.js
+++ app/api/bids/route.js
@@ -1,3 +1,6 @@
+import { NextResponse } from "next/server";
+import xml2js from "xml2js";
+
 export const dynamic = "force-dynamic";
 
 export async function GET() {
@@ -5,17 +8,22 @@ export async function GET() {
   const SERVICE_KEY = process.env.SERVICE_KEY;
 
   const url =
-    `https://apis.data.go.kr/1230000/ad/BidPublicInfoService/getBidPblancListInfoServcPPSSrch` +
+    `https://apis.data.go.kr/1230000/BidPublicInfoService/getBidPblancListInfoServc` +
     `?ServiceKey=${SERVICE_KEY}` +
-    `&numOfRows=10&pageNo=1`;
+    `&numOfRows=100&pageNo=1`;
 
   const res = await fetch(url);
-  const text = await res.text();
+  const xml = await res.text();
 
-  return new Response(text, {
-    headers: {
-      "Content-Type": "text/xml"
-    }
-  });
+  const parser = new xml2js.Parser({ explicitArray:false });
+  const json = await parser.parseStringPromise(xml);
 
+  let items = [];
+
+  if (json?.response?.body?.items?.item) {
+    items = json.response.body.items.item;
+    if (!Array.isArray(items)) items = [items];
+  }
+
+  return NextResponse.json(items);
 }
